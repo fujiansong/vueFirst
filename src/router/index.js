5d4109f4da83fba191daf13cb15224bd97f9578a
fujs <fujs@566560.cn>
2017-12-27 19:52:12 +0800

    Add project

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import index from '@/page/index'
+import Content from '@/page/content'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: index
+    },
+    {
+      path: '/content/:id',
+      component: Content
+    }
+  ]
+})
